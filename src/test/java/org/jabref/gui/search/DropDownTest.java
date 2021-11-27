1327b0ceda5c63c70019dab01348ad678a132147
dmadoery <daniel.madoery@stud.unibas.ch>
2021-11-27 11:53:51 +0100

    Added three test for the DropdownMenu Gui

--- src/test/java/org/jabref/gui/search/DropDownTest.java
+++ src/test/java/org/jabref/gui/search/DropDownTest.java
@@ -1,63 +1,72 @@
 package org.jabref.gui.search;
 
-import javafx.event.Event;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import org.jabref.gui.Globals;
-import org.jabref.gui.JabRefFrame;
-import org.jabref.gui.StateManager;
-import org.jabref.gui.undo.CountingUndoManager;
-import org.jabref.preferences.PreferencesService;
-
+import org.controlsfx.control.PopOver;
 import org.controlsfx.control.textfield.CustomTextField;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(ApplicationExtension.class)
 @Tag("dropdown")
 class DropDownTest {
 
+    private PopOver searchbarDropDown;
+    private Button button;
+    private Button popOverNotShowing;
+    private Button buttonText;
     private CustomTextField searchField;
-    private Stage mainStage;
-    private JabRefFrame frame;
-    private StateManager stateManager;
-    private PreferencesService preferencesService;
-    private CountingUndoManager undoManager;
-    private SearchFieldSynchronizer searchFieldSynchronizer;
-    private GlobalSearchBar globalSearchBar;
-    private DropDownMenu dropDownMenu;
 
-    public void setUp() {
-        this.mainStage = new Stage();
-        this.frame = new JabRefFrame(mainStage);
-        this.stateManager = new StateManager();
-        this.preferencesService = Globals.prefs;
-        this.undoManager = new CountingUndoManager();
-        this.searchFieldSynchronizer = new SearchFieldSynchronizer(searchField);
-        this.globalSearchBar = new GlobalSearchBar(frame, stateManager, preferencesService, undoManager);
-        this.searchField = globalSearchBar.searchField;
-        this.dropDownMenu = new DropDownMenu(searchField, globalSearchBar, searchFieldSynchronizer);
+
+    @Start
+    public void start(Stage stage) {
+        this.searchbarDropDown = new PopOver();
+        this.button = new Button("button");
+        this.popOverNotShowing = new Button("popOverNotShowing");
+        this.buttonText = new Button("WriteSomeText");
+        this.searchField = SearchTextField.create();
+        button.setOnAction(actionEvent -> {
+                searchbarDropDown.show(button);
+        });
+        popOverNotShowing.setOnAction(actionEvent -> {
+                popOverNotShowing.setText("popOverGone");
+                searchbarDropDown.hide();
+        });
+        buttonText.setOnAction(actionEvent -> {
+            searchField.setText("hello");
+        });
+        stage.setScene(new Scene(new StackPane(new HBox(button, popOverNotShowing, buttonText)), 500, 500));
+        stage.show();
     }
 
     @Test
-    void testButtonWorking() {
-        assertEquals("test","test");
+    void testButtonWorking(FxRobot robot) {
+        robot.clickOn(buttonText); //enter Text into searchField
+        assertEquals("hello", searchField.getText());
     }
 
     @Test
-    void testDropDownShowing() {
-        setUp();
-        dropDownMenu.searchbarDropDown.show(dropDownMenu.searchField);
-        Event event = (Event) dropDownMenu.searchField.getOnMouseClicked();
-        dropDownMenu.searchField.fireEvent(event);
-        assertEquals(dropDownMenu.searchbarDropDown.isShowing(), true);
+    void testDropDownShowing(FxRobot robot) {
+        robot.clickOn(button); //popOver is showing
+        assertTrue(searchbarDropDown.isShowing());
     }
 
-
     @Test
-    void bla() {
-        assertTrue(true);
+    void dropDownNotShowing(FxRobot robot) {
+        robot.clickOn(button); //popOver is showing
+        robot.clickOn(popOverNotShowing); //popover hides
+        assertFalse(searchbarDropDown.isShowing());
     }
 }
