61e4ca33195de4797884a9b5749ba9bc9aae253d
Albert <albert.jasari@stud.unibas.ch>
2021-12-08 11:16:27 +0100

    Add AutoCompletionTest.java

--- src/test/java/org/jabref/gui/autocompleter/AutoCompletionTest.java
+++ src/test/java/org/jabref/gui/autocompleter/AutoCompletionTest.java
@@ -5,20 +5,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import org.jabref.gui.search.SearchFieldSynchronizer;
-import org.controlsfx.control.PopOver;
+
 import org.controlsfx.control.textfield.CustomTextField;
 import org.jabref.gui.icon.IconTheme;
-import org.jabref.gui.search.DropDownMenu;
-import org.jabref.gui.search.GlobalSearchBar;
-import org.jabref.gui.search.RecentSearch;
-import org.jabref.gui.search.SearchFieldSynchronizer;
-import org.jabref.gui.search.SearchTextField;
-import org.jabref.model.entry.field.Field;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -35,15 +26,9 @@ import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 
 import static org.jabref.gui.autocompleter.AutoCompleterUtil.getRequest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 @ExtendWith(ApplicationExtension.class)
 @Tag("dropdown")
@@ -77,13 +62,10 @@ class AutoCompletionTest {
 
         stage.setScene(new Scene(new StackPane(new HBox(textfield, addString, deleteButton)), 500, 500));
         stage.show();
-
-
     }
 
     @Test
     void testEntryInDropdownSearchbar(FxRobot robot) {
-
         BibEntry entry = new BibEntry();
         entry.setField(StandardField.TITLE, "testValue");
         database.insertEntry(entry);
@@ -94,10 +76,26 @@ class AutoCompletionTest {
         assertEquals(entry.getFieldValues().toString(), textfield.getText());
     }
 
+    @Test
+    void completeReturnsMultipleResultsInDropdown(FxRobot robot) {
+        String tester = "testValueOne";
+        BibEntry entryOne = new BibEntry();
+        entryOne.setField(StandardField.TITLE, tester);
+        database.insertEntry(entryOne);
+        robot.write(entryOne.getFieldValues().toString());
+        BibEntry entryTwo = new BibEntry();
+        //robot.clickOn(deleteButton);
+        //robot.point(textfield);
+        robot.eraseText(tester.length()+2);
+        entryTwo.setField(StandardField.TITLE, "testValueTwo");
+        database.insertEntry(entryTwo);
+        robot.write(entryTwo.getFieldValues().toString());
+        Collection<String> result = autoCompleter.provideSuggestions(getRequest(("testValue")));
+        assertEquals(Arrays.asList("testValueOne", "testValueTwo"), result);
+    }
 
     @Test
-    void completeWithoutAddingAnythingReturnsSomething(FxRobot robot) {
-
+    void completeWithoutAddingAnythingReturnsSomething(FxRobot robot) { //returns in this example empty ""
         BibEntry entry = new BibEntry();
         entry.setField(StandardField.TITLE, "");
         database.insertEntry(entry);
@@ -106,25 +104,5 @@ class AutoCompletionTest {
         Collection<String> result = autoCompleter.provideSuggestions(getRequest(("")));
         assertEquals(Collections.emptyList(), result);
         assertEquals(entry.getFieldValues().toString(), textfield.getText());
-    }
-
-    @Test
-    void completeReturnsMultipleResultsInDropdown(FxRobot robot) {
-
-        String tester = "testValueOne";
-
-        BibEntry entryOne = new BibEntry();
-        entryOne.setField(StandardField.TITLE, tester);
-        database.insertEntry(entryOne);
-        robot.write(entryOne.getFieldValues().toString());
-        BibEntry entryTwo = new BibEntry();
-      //robot.clickOn(deleteButton);
-      //robot.point(textfield);
-        robot.eraseText(tester.length()+2);
-        entryTwo.setField(StandardField.TITLE, "testValueTwo");
-        database.insertEntry(entryTwo);
-        robot.write(entryTwo.getFieldValues().toString());
-        Collection<String> result = autoCompleter.provideSuggestions(getRequest(("testValue")));
-        assertEquals(Arrays.asList("testValueOne", "testValueTwo"), result);
     }
 }
